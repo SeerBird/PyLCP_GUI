23cca1af6813977a6e3e470a835a012066aae461
SeerBird <ilya.govorunov@gmail.com>
2026-06-02 22:41:57 +0100

    Manifold drag n drop and transitions basics done

--- pylcp_gui/diagram_internals/manifold.py
+++ pylcp_gui/diagram_internals/manifold.py
@@ -15,32 +15,34 @@ class GraphicsDragFilter(QObject):
         self.last_cursor_pos = QPointF()
         self.drag_start_pos = QPointF()
 
-
     def eventFilter(self, watched: QObject, event) -> bool:
-        # Watching the embedded QFrame
-        widget = watched
-        if not isinstance(widget, QFrame):
+        # Watching the embedded Manifold
+        assert isinstance(watched, Manifold)
+        if not isinstance(watched, QFrame):
             return False
 
         # region mouse button press
         if event.type() == QEvent.Type.MouseButtonPress:
-            assert isinstance(event,QMouseEvent)
+            assert isinstance(event, QMouseEvent)
             if event.button() == Qt.MouseButton.LeftButton:
+                # TODO: decide later which parts of the manifold you can drag it by
+                '''
                 # Only drag if clicking empty frame background.
                 # If childAt returns an object, the user clicked an internal button/label.
-                if widget.childAt(event.position().toPoint()) is None:
-                    self.is_pressed = True
-                    self.last_cursor_pos = event.globalPosition()
-                    self.drag_start_pos = event.globalPosition()
-                    event.accept()
-                    return True  # Stop event from reaching child widgets
+                if watched.childAt(event.position().toPoint()) is None:
+                '''
+                self.is_pressed = True
+                self.last_cursor_pos = event.globalPosition()
+                self.drag_start_pos = event.globalPosition()
+                event.accept()
+                return True  # Stop event from reaching child widgets
         # endregion
         # region dragging
         elif event.type() == QEvent.Type.MouseMove and self.is_pressed:
-            assert isinstance(event,QMouseEvent)
+            assert isinstance(event, QMouseEvent)
             if not self.is_dragging:
                 distance = (event.globalPosition() - self.drag_start_pos).manhattanLength()
-                if distance>QApplication.startDragDistance():
+                if distance > QApplication.startDragDistance():
                     self.is_dragging = True
                 return True
             else:
@@ -48,6 +50,7 @@ class GraphicsDragFilter(QObject):
                 self.proxy.setPos(self.proxy.pos() + delta)
                 self.last_cursor_pos = event.globalPosition()
                 self.is_dragging = True
+                watched.positionChanged.emit()
                 event.accept()
                 return True
         # endregion
@@ -64,29 +67,24 @@ class GraphicsDragFilter(QObject):
         # endregion
         return super().eventFilter(watched, event)
 
+
 class Manifold(QFrame):
     clicked = Signal()
-    positionChanged = Signal(QPointF)
+    positionChanged = Signal()
 
     def __init__(self, label: str, energy: float):
         # TODO: maybe an energy, maybe an F-number,
         super().__init__()
         self.label = label
         self.energy = energy
-        # region the QWidget container this object is a proxy of
-        self.container = QFrame()
-        self.layout = QGridLayout(self.container)
+        self.layout = QGridLayout(self)
         self.layout.addWidget(QLabel(label))
         self.layout.addWidget(QLabel(f"H_0: {energy:.3E}"))
         self.setFrameShape(QFrame.Shape.HLine)  # TODO: make this contain multiple states
         self.setFixedSize(config.manifold_height, config.manifold_width)
-
-        # endregion
-
 
     def mouseReleaseEvent(self, event, /):
         if event.button() == Qt.MouseButton.LeftButton:
             self.clicked.emit()
             return
         super().mouseReleaseEvent(event)
-
